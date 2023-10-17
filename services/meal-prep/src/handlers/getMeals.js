551d1f7c0ade28a28f0a81e9a371babcdabcf10e
Kim Lu <{username}@users.noreply.github.com>
2023-10-17 15:11:40 -0400

    using lambda powertools logger dependency

--- services/meal-prep/src/handlers/getMeals.js
+++ services/meal-prep/src/handlers/getMeals.js
@@ -1,8 +1,12 @@
 const { getItem } = require("../helpers/dynamo");
 const { buildResponse, errorResponse } = require("../helpers/response");
+const { Logger } = require("@aws-lambda-powertools/logger");
+
+const logger = new Logger({ serviceName: 'MealPrepService' });
 const TableName = process.env.DYNAMODB_TABLE;
 
 const handler = async (event) => {
+    logger.info(event)
     try {
         const id = event.pathParameters?.id;
 
